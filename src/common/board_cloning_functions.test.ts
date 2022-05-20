37bf63ee8356f1c090fc3f19304b4d9a31e06a67
David Martín-González <3705952+unpollito@users.noreply.github.com>
2022-05-20 16:08:22 +0200

    Complete tests for common functions, add tests to pre-commit hook

--- src/common/board_cloning_functions.test.ts
+++ src/common/board_cloning_functions.test.ts
@@ -1,5 +1,6 @@
-import { cloneCells } from "./board_cloning_functions";
+import { cloneCells, cloneCellsAround } from "./board_cloning_functions";
 import { generateExampleBoard } from "./common_test_functions";
+import { GameBoard, GameCell } from "./types";
 
 describe("cloneCells", () => {
   const board = generateExampleBoard(4, 4);
@@ -33,5 +34,97 @@ describe("cloneCells", () => {
 });
 
 describe("cloneCellsAround", () => {
-  // TODO
+  const testForRadius = ({
+    around,
+    board,
+    radius,
+  }: {
+    around: GameCell;
+    board: GameBoard;
+    radius: number;
+  }): void => {
+    const clonedCells = cloneCellsAround({
+      around,
+      cells: board.cells,
+      radius,
+    });
+    const minRowToClone = Math.max(0, around.rowIndex - radius);
+    const maxRowToClone = Math.min(
+      board.cells.length,
+      around.rowIndex + radius
+    );
+    const minColumnToClone = Math.max(0, around.columnIndex - radius);
+    const maxColumnToClone = Math.min(
+      board.cells[0].length,
+      around.columnIndex + radius
+    );
+
+    it("returns a matrix whose cells are equal", () => {
+      for (const [rowIndex, row] of board.cells.entries()) {
+        for (const [columnIndex, cell] of row.entries()) {
+          const clonedCell = clonedCells[rowIndex][columnIndex];
+          expect(clonedCell.rowIndex).toBe(rowIndex);
+          expect(clonedCell.columnIndex).toBe(columnIndex);
+          expect(clonedCell).toEqual(cell);
+        }
+      }
+    });
+
+    it(`returns a matrix where only the cells outside a ${radius} cell radius have equal references`, () => {
+      for (const [rowIndex, row] of board.cells.entries()) {
+        for (const [columnIndex, cell] of row.entries()) {
+          const isRowNonClonable =
+            rowIndex < minRowToClone || rowIndex > maxRowToClone;
+          const isColumnNonClonable =
+            columnIndex < minColumnToClone || columnIndex > maxColumnToClone;
+          const clonedCell = clonedCells[rowIndex][columnIndex];
+          expect(clonedCell === cell).toBe(
+            isRowNonClonable || isColumnNonClonable
+          );
+        }
+      }
+    });
+  };
+
+  describe("with radius = 0", () => {
+    const board = generateExampleBoard(4, 4);
+    const around = board.cells[1][1];
+    testForRadius({ around, board, radius: 0 });
+  });
+
+  describe("with radius = 1 in the center", () => {
+    const board = generateExampleBoard(5, 5);
+    const around = board.cells[2][2];
+    testForRadius({ around, board, radius: 1 });
+  });
+
+  describe("with radius = 1 in the top left corner", () => {
+    const board = generateExampleBoard(5, 5);
+    const around = board.cells[0][0];
+    testForRadius({ around, board, radius: 1 });
+  });
+
+  describe("with radius = 1 in the bottom right corner", () => {
+    const board = generateExampleBoard(5, 5);
+    const around = board.cells[4][4];
+    testForRadius({ around, board, radius: 1 });
+  });
+
+  describe("with radius = 2 in the center", () => {
+    const board = generateExampleBoard(7, 7);
+    const around = board.cells[3][3];
+    testForRadius({ around, board, radius: 2 });
+  });
+
+  describe("with radius = 2 in the top right corner", () => {
+    const board = generateExampleBoard(7, 7);
+    const around = board.cells[0][6];
+    testForRadius({ around, board, radius: 2 });
+  });
+
+  describe("with radius = 2 in the bottom left corner", () => {
+    const board = generateExampleBoard(7, 7);
+    const around = board.cells[6][0];
+    testForRadius({ around, board, radius: 2 });
+  });
 });
